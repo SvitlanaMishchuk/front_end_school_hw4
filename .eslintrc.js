ab978054839e501d931dbecb2657ef231d5e35fb
Svitlana Mishchuk <svitlana@igtechn.com>
2022-02-04 16:07:51 +0700

    chore(app): removed unused files, added packages

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'react/function-component-definition': [0],
+  },
+};
